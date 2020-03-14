c10db20c50dc94282d89280410af701a1fe153ad
olitwenty <o.eschrich@googlemail.com>
2020-03-14 16:33:14 +0100

    some changes

--- zinsrechner.py
+++ zinsrechner.py
@@ -1,31 +1,51 @@
 #!/usr/bin/env python3
 
+import os
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
 
-parser.add_argument("-d", "--darlehen", dest="darlehen", nargs=1, type=int, required=True, help="Größe des Darlehens")
-parser.add_argument("-z", "--zinssatz", dest="zinssatz", nargs=1, type=float, required=True, help="Zinssatz")
-parser.add_argument("-t", "--tilgung", dest="tilgung", nargs=1, type=float, required=True, help="Anfängliche jährliche Tilgung")
+parser.add_argument("-d", "--darlehen", dest="darlehen", nargs=2, type=int, required=True, help="Größe des Darlehens")
+parser.add_argument("-z", "--zinssatz", dest="zinssatz", nargs=2, type=float, required=True, help="Zinssatz")
+parser.add_argument("-t", "--tilgung", dest="tilgung", nargs=2, type=float, required=True, help="Anfängliche jährliche Tilgung")
 
 args = parser.parse_args()
-darlehen = args.darlehen[0]
-zinssatz = args.zinssatz[0] / 100
-tilgung = args.tilgung[0] /100
-rate = (zinssatz + tilgung) * darlehen / 12
+maxDarlehen = args.darlehen[0]
+minDarlehen = args.darlehen[1]
+maxZinssatz = args.zinssatz[0] / 100
+minZinssatz = args.zinssatz[1] / 100
+maxTilgung = args.tilgung[0] /100
+minTilgung = args.tilgung[0] /100
 
-print(rate)
 
-def rek(darlehen, jahre):
-    if darlehen < rate:
-        return jahre
+f = "Jahr: {:<15}Restschuld: {:<15.2f}Tilgung: {:<15.2f}"
+
+def orange(start, stop, step):
+    pass
+
+def rek(darlehen, zinssatz, jahre):
+    if darlehen < rate*12:
+        print(f.format(jahre, darlehen, darlehen))
+        return
     else:
-        print(jahre, darlehen, rate-darlehen*zinssatz)
-        return rek((darlehen - (rate * 12 - (darlehen * zinssatz))), jahre + 1)
+        print(f.format(jahre, darlehen, rate*12-darlehen*zinssatz))
+        return rek((darlehen - (rate * 12 - (darlehen * zinssatz))), zinssatz, jahre + 1)
     
-rek(darlehen, 0)
+for d in range(maxDarlehen, minDarlehen, -10000):
+    for z in range(maxZinssatz, minZinssatz, -0.5):
+        for t in range(maxTilgung, minTilgung, -0.5):
+            rate = (z + t) * d / 12
+            print(rate)
+            rek(d, z, 0)
 
-print(darlehen)
+#os.mkdir("test")
+#os.mkdir("test/abc")
+#f = open("test/"+str(maxDarlehen)+".txt", "w", newline="\n")
+#f.write("jnljnjkbnl\n")
+#f.write("kshvksajjsd")
+#f.close()
+
+print(maxDarlehen)
 print(zinssatz)
 print(tilgung)
-print(darlehen * ((zinssatz + tilgung) / 100) / 12)
+print(maxDarlehen * ((zinssatz + tilgung) / 100) / 12)
